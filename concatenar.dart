e16de3978c09c38d06d9deefb65b335ee0034106
Brian Marquez I <brian3marquez@gmail.com>
2022-05-02 18:27:10 +0000

    Concatenar and OperatorLogic

---
+++ concatenar.dart
@@ -0,0 +1,14 @@
+
+// Main function
+void main() {
+   
+  // Assigning values
+  // to the variable
+  String uno = " brian ";
+  String dos = " marquez ";
+  String tres = " inca roca ";
+   
+  // Printing concatenated result
+  // by the use of '+' operator
+  print(uno + dos + tres);
+}
--- operadoresAsignaciones.dart
+++ operadoresAsignaciones.dart
@@ -1,11 +1,18 @@
 // Operador de Asignaciones
+//https://www.geeksforgeeks.org/how-to-append-or-concatenate-strings-in-dart/
 
 void main() {
-  var data1 = 9.0;
+  //var data1 = 9.0;
+  var data1 = 9;
   var data2 = 2;
 
-  var value = data1 / data2;
-  print(value);
-}
+  //var value = data1 ~/ data2;
+  var value = data1 ~/ data2; //value 4, valor tipo entero
+  //print('data1 ->: ' value); 
+  print("..........................................................");
+  // Operador Especial
 
+  data1 ~/= data2; // hace la dicion y lo asigna a data1
+  print('data1 ->: $data1');
+}
 
