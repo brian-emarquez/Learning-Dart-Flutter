04d1c912010bb752e25b20607001a5c55e0c4fcc
Brian Marquez I <brian3marquez@gmail.com>
2022-05-02 20:31:24 +0000

    OperatorLogic 2

---
+++ operadoresLogicos2.dart
@@ -0,0 +1,14 @@
+// Operador de Logicos 2
+
+operador_logico(){
+
+  var data1 = 5;
+  var data2 = 2;
+  var value = data1 == data2 ? "iguales" : "no iguales";
+  print(value);
+  
+}
+void main() {
+  operador_logico();
+}
+
