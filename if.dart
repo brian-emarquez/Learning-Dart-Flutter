a15e2a9b697d0ddf8ac5e0aeaf1e9b84281cd228
Brian Marquez I <brian3marquez@gmail.com>
2022-05-02 21:59:44 +0000

    If

---
+++ if.dart
@@ -0,0 +1,21 @@
+// IF
+
+operador_if(){
+
+var data1 = 10;
+var value = "";
+  
+if(5 > data1){
+  value = "repositorio de dart y flutter";
+  print(value);
+}
+else{
+  value = "error";
+  print(value);
+  }
+}
+
+void main() {
+
+  operador_if();
+}
