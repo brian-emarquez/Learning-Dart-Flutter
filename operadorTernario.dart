cf6e838741cc1a23f72f2d7602df2253844a4cef
BrianMarquez3 <brian3marquez@gmail.com>
2022-05-02 18:53:47 -0500

    operador ternario dark

---
+++ operadorTernario.dart
@@ -0,0 +1,40 @@
+// Operador Ternário
+
+operador_if() {
+
+  var data1 = 10;
+  var value = "";
+    
+  if(5 > data1){
+    value = "repositorio de dart y flutter";
+    print(value);
+  }
+  else{
+    value = "error";
+    print(value);
+    }
+}
+
+operador_ternario(){
+  
+  var data1 = 10;
+  var value = "";
+    
+  if(5 > data1){
+    value = "repositorio de dart y flutter";
+    print(value);
+  }
+  else{
+    value = "error";
+    print(value);
+    
+}
+
+void main() {
+
+  //operador_if();
+  operador_ternario()
+
+}
+
+
