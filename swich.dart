4b538ff8142fbfed08fd8940d262726bbf341672
Brian Marquez I <brian3marquez@gmail.com>
2022-05-03 17:42:47 +0000

    Switch update

--- swich.dart
+++ swich.dart
@@ -10,7 +10,36 @@ switch(paisHablaHispana){
     break;
 
     case "Francia":
+      print("Incorrecto");// Switch in dart
+
+add_switch(){
+
+String paisHablaHispana = "Perúl000";
+
+switch(paisHablaHispana){
+    case ("Canada"):
       print("Incorrecto");
+    break;
+
+    case "Francia":
+      print("Incorrecto");
+    break;
+    
+    case "Perú":
+      print("Correcto");
+    break;
+
+    default:
+      //print("ejecutaste el default");
+      throw "mimguna opcion no valida"; //excepcion
+  }
+}
+
+void main() {
+  add_switch();
+}
+
+
     break;
     
     case "Perú":
