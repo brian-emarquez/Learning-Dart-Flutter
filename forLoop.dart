398d887b83d72a546519f69422cd5e77c867c525
Brian Marquez I <brian3marquez@gmail.com>
2022-05-03 17:47:57 +0000

    For Loop

---
+++ forLoop.dart
@@ -0,0 +1,36 @@
+// ForLoop
+
+add_switch(){
+
+  String paisHablaHispana = "Perúl000";
+
+  switch(paisHablaHispana){
+      case ("Canada"):
+        print("Incorrecto");
+      break;
+
+      case "Francia":
+        print("Incorrecto");
+      break;
+      
+      case "Perú":
+        print("Correcto");
+      break;
+
+      default:
+        //print("ejecutaste el default");
+        throw "mimguna opcion no valida"; //excepcion
+    }
+}
+
+forLoop(){
+
+}
+
+
+
+void main() {
+  //add_switch();
+  forLoop();
+}
+
--- swich.dart
+++ swich.dart
@@ -2,18 +2,6 @@
 
 add_switch(){
 
-String paisHablaHispana = "Perú";
-
-switch(paisHablaHispana){
-    case ("Canada"):
-      print("Incorrecto");
-    break;
-
-    case "Francia":
-      print("Incorrecto");// Switch in dart
-
-add_switch(){
-
 String paisHablaHispana = "Perúl000";
 
 switch(paisHablaHispana){
@@ -32,19 +20,6 @@ switch(paisHablaHispana){
     default:
       //print("ejecutaste el default");
       throw "mimguna opcion no valida"; //excepcion
-  }
-}
-
-void main() {
-  add_switch();
-}
-
-
-    break;
-    
-    case "Perú":
-      print("Correcto");
-    break;
   }
 }
 
