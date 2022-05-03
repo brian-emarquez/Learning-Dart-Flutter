97d04bfe6bde6784e24c1f394323614cf209b4ef
9780desarrollador07 <9780desarrollador07@gmail.com>
2022-05-03 12:36:37 -0500

    Switch

---
+++ swich.dart
@@ -0,0 +1,25 @@
+// Switch in dart
+
+add_switch(){
+
+String paisHablaHispana = "Perú";
+
+switch(paisHablaHispana){
+    case ("Canada"):
+      print("Incorrecto");
+    break;
+
+    case "Francia":
+      print("Incorrecto");
+    break;
+    
+    case "Perú":
+      print("Correcto");
+    break;
+  }
+}
+
+void main() {
+  add_switch();
+}
+
