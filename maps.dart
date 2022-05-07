ee5cdac5a7b3eede47f241fc6ef5bbfddc1e31e5
Brian Marquez I <brian3marquez@gmail.com>
2022-05-07 14:17:56 +0000

    Maps Update

--- maps.dart
+++ maps.dart
@@ -1,12 +1,17 @@
 // Mapts
+import 'dart:convert';
+
 void miMaps(){
-  var groups = {
+  var groups = <String, String>{
       "perro": "animal",
       "Bus": "vehiculo"
   };
   //print(groups["perro"]);
-  groups.forEach((key, value) => print("$key -> $value"));
-  print(groups.keys);
+  //groups.forEach((key, value) => print("$key -> $value"));
+
+  print(groups["perro"]);
+
+  // print(groups.keys);
 }
 
 
